c3edaea465202049adb8549f4765c986c55754ac
golsharifi <nima@orb.group>
2024-06-04 10:14:54 -0500

    Update

---
+++ docusaurus.config.ts
@@ -0,0 +1,223 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'OrbVPN Resource Center',
+  tagline: 'The OrbVPN Resource Center is your go-to destination for comprehensive guides, tutorials, and expert advice to maximize your online privacy and security with OrbVPN.',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://docs.orbvpn.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'OrbVPN', // Usually your GitHub org/user name.
+  projectName: 'OrbVPN Resource Center', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fa'],
+    path: 'i18n',
+    localeConfigs: {
+      en: {
+        label: 'English',
+        direction: 'ltr',
+        htmlLang: 'en-US',
+        calendar: 'gregory',
+        path: 'en',
+      },
+      fa: {
+        label: 'فارسی',
+        direction: 'rtl',
+        htmlLang: 'fa-IR',
+        calendar: 'persian',
+        path: 'fa',
+      },
+    },
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'IZZT1AVCRI',
+
+      // Public API key: it is safe to commit it
+      apiKey: '057c1a9353cae106544caf567cf84d25',
+
+      indexName: 'orbvpn',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      //... other Algolia params
+    },
+
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'OrbVPN',
+      logo: {
+        alt: 'OrbVPN Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Guides',
+        },
+
+        {
+          type: 'docSidebar',
+          sidebarId: 'troubleshootSidebar',
+          position: 'left',
+          label: 'Troubleshoot',
+        },
+
+        {
+          type: 'docSidebar',
+          sidebarId: 'apiSidebar',
+          position: 'left',
+          label: 'API',
+        },
+        
+        {to: '/blog', label: 'Blog', position: 'left'},
+
+        {
+          href: 'https://orbvpn.xyz',
+          label: 'Login',
+          position: 'right',
+        },
+
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+        
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Guides',
+          items: [
+            {
+              label: 'iOS',
+              to: '/docs/intro',
+            },
+            {
+              label: 'Android',
+              to: '/docs/intro',
+            },
+            {
+              label: 'Windows',
+              to: '/docs/intro',
+            },
+            {
+              label: 'MacOS',
+              to: '/docs/intro',
+            },
+
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'X',
+              href: 'https://twitter.com/orbvpn',
+            },
+            {
+              label: 'LinkedIn',
+              href: 'https://www.linkedin.com/company/orbvpn/',
+            },
+            {
+              label: 'Instagram',
+              href: 'https://instagram.com/orbvpn',
+            },
+            {
+              label: 'Youtube',
+              href: 'https://youtube.com/orbvpn',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'User Dashboard',
+              href: 'https://orbvpn.xyz',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} NIMA OÜ.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
